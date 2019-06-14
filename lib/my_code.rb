08bc011598ae72eeceb084b9c5f26425a6df6531
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-14 22:41:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,2 @@
-def some_method(map_to_negativize)
-    map_to_negativize.map do |x|
-      if x > 10
-         return x+1
-      else
-         return x-1
-      end
-    end
-end
+map_to_negativize = [2,3,-4,5]
+array.map { |integer| string.upcase }
